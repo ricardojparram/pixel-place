468daf0dabe4ac914aeb94f73328407c9ea879d3
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-12-10 20:15:54 -0400

    feat: project initialized

---
+++ front-end/src/components/Canvas.vue
@@ -0,0 +1,29 @@
+<template><canvas id="canvas" width="4000" height="2000"></canvas></template>
+<
+
+<script setup>
+import { onMounted } from 'vue'
+onMounted(() => {
+  console.log('hola')
+  const canvas = document.getElementById('canvas')
+  console.log(canvas)
+  const c2d = canvas.getContext('2d')
+  c2d.strokeStyle = '#cbd5e1'
+
+  //   c2d.fillStyle = 'red'
+  //   c2d.fillRect(50, 0, 50, 50)
+
+  for (var i = 0; i < canvas.width; i += 20) {
+    c2d.beginPath()
+    c2d.moveTo(i, 0)
+    c2d.lineTo(i, canvas.height)
+    c2d.stroke()
+  }
+  for (var i = 0; i < canvas.height; i += 20) {
+    c2d.beginPath()
+    c2d.moveTo(0, i)
+    c2d.lineTo(canvas.width, i)
+    c2d.stroke()
+  }
+})
+</script>
